4510b348be5c41346f7403d64e84b44b4c946f1d
Jamesburgess44 <jamesburgess44@yahoo.com>
2022-02-20 23:38:58 -0700

    updated first three cards in the project section

--- my-portfolio/src/components/projects/projects.jsx
+++ my-portfolio/src/components/projects/projects.jsx
@@ -1,6 +1,6 @@
 import './projects.css'
 import { Container, Card, CardGroup, Button } from 'react-bootstrap';
-import rvdphoto from './rvdcodesnippet.png'
+
 
 const Projects = (props) => {
     return (
@@ -12,29 +12,33 @@ const Projects = (props) => {
                         <Card.Text>
                         The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
                         </Card.Text>
-                    <Button variant="dark">Visit Github</Button>
+                    <Button className="left-button" variant="dark">Visit Github</Button>
+                    <Button className="right-button" variant="dark">Visit Github</Button>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img  class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
                 </Card.Footer>
             </Card>
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
+                    <Card.Title>Rock Paper Sciccor Lizard Spock</Card.Title>
                         <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
+                        Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, create a console version of the classic game Rock Paper Scissors Lizard Spock.
                         </Card.Text>
-                    <Button variant="dark">Visit Github</Button>
+                        <Button className="left-button" variant="dark">Visit Github</Button>
+                    <Button className="right-button" variant="dark">Visit Github</Button>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
                 </Card.Footer>
             </Card>
             <Card className="text-center">
                     <Card.Body>
                     <Card.Title>Robots VS Dinosaurs</Card.Title>
                         <Card.Text>
-                        With Object Oriented Programming, creating classes to represent the unique data structures in our program, creating instances (objects) from those classes, and having those objects interact with each other.
+                        With Object Oriented Programming, creating instances (objects) from those classes, and having those objects interact with each other.
                         </Card.Text>
                         <Button variant="dark">Visit Github</Button>
                 </Card.Body>
@@ -46,14 +50,16 @@ const Projects = (props) => {
                 <CardGroup className="row">
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
+                    <Card.Title>Rock Paper Sciccor Lizard Spock</Card.Title>
                         <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
+                        Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, create a console version of the classic game Rock Paper Scissors Lizard Spock.
                         </Card.Text>
-                    <Button variant="dark">Visit Github</Button>
+                        <Button className="left-button" variant="dark">Visit Github</Button>
+                    <Button className="right-button" variant="dark">Visit Github</Button>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
                 </Card.Footer>
             </Card>
             <Card className="text-center">
@@ -72,7 +78,7 @@ const Projects = (props) => {
                     <Card.Body>
                     <Card.Title>Robots VS Dinosaurs</Card.Title>
                         <Card.Text>
-                        With Object Oriented Programming, creating classes to represent the unique data structures in our program, creating instances (objects) from those classes, and having those objects interact with each other.
+                        With Object Oriented Programming, creating instances (objects) from those classes, and having those objects interact with each other.
                         </Card.Text>
                         <Button variant="dark">Visit Github</Button>
                 </Card.Body>
