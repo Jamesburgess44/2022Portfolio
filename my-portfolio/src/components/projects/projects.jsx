164560226d90368deac97b9d57a8e01cb299e87f
Jamesburgess44 <jamesburgess44@yahoo.com>
2022-04-17 14:32:50 -0600

    added digital assets for products page

--- my-portfolio/src/components/projects/projects.jsx
+++ my-portfolio/src/components/projects/projects.jsx
@@ -1,5 +1,16 @@
 import './projects.css'
 import { Container, Card, CardGroup, Button } from 'react-bootstrap';
+import CCCenter from './CaraCaraCenter.jpg'
+import CCLeft from './CaraCaraLeft.png'
+import CCRight from './CaraCaraRight.png'
+import DPLeft from './DaniPortfolioLeft.png'
+import DPRight from './DaniPortfolioRight.png'
+import DPCenter from './DaniPortfolioCenter.png'
+import CSLeft from './CardShopLeft.png'
+import CSRight from './CardShopRight.png'
+import CSCenter from './CardShopCenter.png'
+
+
 
 
 const Projects = (props) => {
@@ -9,12 +20,7 @@ const Projects = (props) => {
                {/* start card one  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
-                        <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
-                        </Card.Text>
-                    <Button className="left-button" variant="dark">Visit Github</Button>
-                    <Button className="right-button" variant="dark">Visit Github</Button>
+                    <img className='project_image' src={CCLeft}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img  class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
@@ -25,12 +31,7 @@ const Projects = (props) => {
             {/* start card two  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Rock Paper Sciccor Lizard Spock</Card.Title>
-                        <Card.Text>
-                        Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, create a console version of the classic game Rock Paper Scissors Lizard Spock.
-                        </Card.Text>
-                        <Button className="left-button" variant="dark">Visit Github</Button>
-                    <Button className="right-button" variant="dark">Visit Github</Button>
+                    <img className='project_image' src={CCCenter}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
@@ -41,11 +42,7 @@ const Projects = (props) => {
             {/* start card three  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Robots VS Dinosaurs</Card.Title>
-                        <Card.Text>
-                        With Object Oriented Programming, creating instances (objects) from those classes, and having those objects interact with each other.
-                        </Card.Text>
-                        <Button variant="dark">Visit Github</Button>
+                    <img className='project_image' src={CCRight}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
@@ -57,12 +54,7 @@ const Projects = (props) => {
                 {/* start card four  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Rock Paper Sciccor Lizard Spock</Card.Title>
-                        <Card.Text>
-                        Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, create a console version of the classic game Rock Paper Scissors Lizard Spock.
-                        </Card.Text>
-                        <Button className="left-button" variant="dark">Visit Github</Button>
-                    <Button className="right-button" variant="dark">Visit Github</Button>
+                        <img className='project_image' src={DPLeft}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
@@ -73,11 +65,7 @@ const Projects = (props) => {
             {/* start card five  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
-                        <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
-                        </Card.Text>
-                    <Button variant="dark">Visit Github</Button>
+                    <img className='project_image' src={DPCenter}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
@@ -87,11 +75,7 @@ const Projects = (props) => {
             {/* start card six  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Robots VS Dinosaurs</Card.Title>
-                        <Card.Text>
-                        With Object Oriented Programming, creating instances (objects) from those classes, and having those objects interact with each other.
-                        </Card.Text>
-                        <Button variant="dark">Visit Github</Button>
+                    <img className='project_image' src={DPRight}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
@@ -103,11 +87,7 @@ const Projects = (props) => {
                 {/* start card seven  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
-                        <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
-                        </Card.Text>
-                    <Button variant="dark">Visit Github</Button>
+                        <img className='project_image' src={CSLeft}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
@@ -117,11 +97,7 @@ const Projects = (props) => {
             {/* start card eight  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
-                        <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
-                        </Card.Text>
-                    <Button variant="dark">Visit Github</Button>
+                    <img className='project_image' src={CSCenter}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
@@ -131,11 +107,7 @@ const Projects = (props) => {
             {/* start card nine  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Robots VS Dinosaurs</Card.Title>
-                        <Card.Text>
-                        With Object Oriented Programming, creating classes to represent the unique data structures in our program, creating instances (objects) from those classes, and having those objects interact with each other.
-                        </Card.Text>
-                        <Button variant="dark">Visit Github</Button>
+                    <img className='project_image' src={CSRight}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
                     <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
