bfc8892ee01e5ef9c07c86eee1dde41c1878b29e
Jamesburgess44 <jamesburgess44@yahoo.com>
2022-04-18 19:01:40 -0600

    added detials to the project tables

--- my-portfolio/src/components/projects/projects.jsx
+++ my-portfolio/src/components/projects/projects.jsx
@@ -17,118 +17,122 @@ const Projects = (props) => {
     return (
         <Container class="bg-black">
            <CardGroup className="row">
+               <h1 className="text-center">Ecommerce design</h1>
                {/* start card one  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={CCLeft}/>
+                    <img alt="" className='project_image' src={CCLeft}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img  class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                    <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" /> 
                 </Card.Footer>
             </Card>
             {/* end card one  */}
             {/* start card two  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={CCCenter}/>
+                    <img alt="" className='project_image' src={CCCenter}/>
                 </Card.Body>
-                <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
-                </Card.Footer>
+                    <Card.Footer className="text-muted">
+                    <a href="https://github.com/Jamesburgess44/CaraCara"><Button className="left-button" variant="dark">Visit Github</Button></a>
+                    </Card.Footer>
             </Card>
             {/* end card two  */}
             {/* start card three  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={CCRight}/>
+                    <img alt="" className='project_image' src={CCRight}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
+                    <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />    
                 </Card.Footer>
             </Card>
             {/* end card three  */}
                 </CardGroup>
                 <CardGroup className="row">
+                <h1 className="text-center">Portfolio Design</h1>
                 {/* start card four  */}
             <Card className="text-center">
                     <Card.Body>
-                        <img className='project_image' src={DPLeft}/>
+                        <img alt="" className='project_image' src={DPLeft}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
+                    <span>Frontend</span>
+                <img  alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
                 </Card.Footer>
             </Card>
             {/* end card four  */}
             {/* start card five  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={DPCenter}/>
+                    <img alt="" className='project_image' src={DPCenter}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                <a href="https://github.com/Jamesburgess44/dani_p_react_website"><Button className="left-button" variant="dark">Visit Github</Button> </a>
                 </Card.Footer>
             </Card>
             {/* end card five  */}
             {/* start card six  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={DPRight}/>
+                    <img alt="" className='project_image' src={DPRight}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
+                    <span>Backend</span>
+                <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />   
                 </Card.Footer>
             </Card>
             {/* end card six  */}
                 </CardGroup>
                 <CardGroup className="row">
+                <h1 className="text-center">eShop Design</h1>
                 {/* start card seven  */}
             <Card className="text-center">
                     <Card.Body>
-                        <img className='project_image' src={CSLeft}/>
+                        <img alt="" className='project_image' src={CSLeft}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                <span>Frontend</span>
+                <img  alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
                 </Card.Footer>
             </Card>
             {/* end card seven  */}
             {/* start card eight  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={CSCenter}/>
+                    <img alt="" className='project_image' src={CSCenter}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                <a href="https://github.com/Jamesburgess44/Baseball-Card-Store"><Button className="left-button" variant="dark">Visit Github</Button></a>
                 </Card.Footer>
             </Card>
             {/* end card eight  */}
             {/* start card nine  */}
             <Card className="text-center">
                     <Card.Body>
-                    <img className='project_image' src={CSRight}/>
+                    <img alt="" className='project_image' src={CSRight}/>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
+                <span>Backend</span>
+                <img class="logo" alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />    
                 </Card.Footer>
             </Card>
             {/* end card nine  */}
                 </CardGroup>
            <CardGroup className="row">
+               <h1 className="text-center">Terminal/Console Projects</h1>
                 {/* start card ten  */}
             <Card className="text-center">
                     <Card.Body>
-                    <Card.Title>Day Trip Generator</Card.Title>
+                    <Card.Title>Most Wanted</Card.Title>
                         <Card.Text>
-                        The Day Trip Generator will allow a user to have a random trip generated for them, making it less of a hassle to come up with trip ideas for the day!
+                        A project that allows for you to search for people by their name or physical attributes using higher order array methods and then display their information. 
                         </Card.Text>
-                    <Button className="left-button" variant="dark">Visit Github</Button>
-                    <Button className="right-button" variant="dark">Visit Github</Button>
+                    <a href="https://github.com/Jamesburgess44/Most-Wanted"><Button className="right-button" variant="dark">Visit Github</Button></a>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img  class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+                    <img alt=""  class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
                 </Card.Footer>
             </Card>
             {/* end card ten  */}
@@ -139,12 +143,12 @@ const Projects = (props) => {
                         <Card.Text>
                         Using the concepts of OOP by creating classes and using objects (instances of those classes) to interact with each other, create a console version of the classic game Rock Paper Scissors Lizard Spock.
                         </Card.Text>
-                        <Button className="left-button" variant="dark">Visit Github</Button>
-                    <Button className="right-button" variant="dark">Visit Github</Button>
+                        <a href="https://github.com/Jamesburgess44/RPSLS"><Button className="left-button" variant="dark">Visit Github</Button></a>
+                    <a href="https://github.com/Jamesburgess44/RPSLS-Csharp"><Button className="right-button" variant="dark">Visit Github</Button></a>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
+                    <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+                    <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg" />
                 </Card.Footer>
             </Card>
             {/* end card eleven  */}
@@ -155,10 +159,10 @@ const Projects = (props) => {
                         <Card.Text>
                         With Object Oriented Programming, creating instances (objects) from those classes, and having those objects interact with each other.
                         </Card.Text>
-                        <Button variant="dark">Visit Github</Button>
+                        <a href="https://github.com/Jamesburgess44/RobotsvsDinosaurs"><Button variant="dark">Visit Github</Button></a>
                 </Card.Body>
                 <Card.Footer className="text-muted">
-                    <img class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
+                    <img alt="" class="logo" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />    
                 </Card.Footer>
             </Card>
             {/* end card twelve  */}
